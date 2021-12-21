9badf4cb44078fbd25ceae090d88f8147e7e21a2
Chris Smith <csmith@platformconsulting.io>
2021-12-21 16:25:48 -0500

    Implement HTTP for more operations decidinging matches updating elo getting player information

--- main.go
+++ main.go
@@ -7,10 +7,16 @@ import (
 	"net/http"
 )
 
+type Match struct {
+	PlayerA, PlayerB, result string
+}
+
 var operator elocaluclaton.Operator
 
+var resultsMap map[string]int
+
 /*
-	Adds players to the registry based on input given
+	Adds one or more players to the registry based on input given
 */
 
 func AddPlayers(w http.ResponseWriter, r *http.Request) {
@@ -18,6 +24,8 @@ func AddPlayers(w http.ResponseWriter, r *http.Request) {
 
 	err := json.NewDecoder(r.Body).Decode(&p)
 	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+
 		return
 	}
 	println("Players retrived ", len(p))
@@ -36,7 +44,61 @@ func AddPlayers(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func Decide(w http.ResponseWriter, r *http.Request) {
+	var m Match
+
+	err := json.NewDecoder(r.Body).Decode(&m)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	err = operator.RunMatch(m.PlayerA, m.PlayerB, resultsMap[m.result])
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+}
+
+func UpdatePlayerElo(w http.ResponseWriter, r *http.Request) {
+	var p elocaluclaton.Player
+	err := json.NewDecoder(r.Body).Decode(&p)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	err = operator.UpdateEloForPlayer(p.Tag, p.Elo)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+}
+
+func RetrievePlayerInformation(w http.ResponseWriter, r *http.Request) {
+	var p elocaluclaton.Player
+	err := json.NewDecoder(r.Body).Decode(&p)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	p, err = operator.RetrievePlayerInformation(p.Tag)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	err = json.NewEncoder(w).Encode(p)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+}
+
 func main() {
+	resultsMap := make(map[string]int)
+	resultsMap["WIN"] = elocaluclaton.WIN
+	resultsMap["LOSS"] = elocaluclaton.LOSS
+	resultsMap["TIE"] = elocaluclaton.TIE
+
 	operator = elocaluclaton.CreateInMemoryOperator()
 	operator.CreateNewPlayer("chris", 1000)
 	operator.CreateNewPlayer("paul", 1000)
@@ -48,6 +110,9 @@ func main() {
 	operator.RetrievePlayerInformation("chris")
 
 	http.HandleFunc("/add", AddPlayers)
+	http.HandleFunc("/decide", Decide)
+	http.HandleFunc("/updateElo", UpdatePlayerElo)
+	http.HandleFunc("/getPlayer", RetrievePlayerInformation)
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
