2b6ac0d339869b8fecf87f59e8a6cd079d774ac1
Chris Smith <csmith@platformconsulting.io>
2021-12-21 14:56:00 -0500

    Add handling Add Player via HTTP request

--- main.go
+++ main.go
@@ -2,9 +2,39 @@ package main
 
 import (
 	"PlayerElo/elocaluclaton"
+	"encoding/json"
+	"log"
+	"net/http"
 )
 
 var operator elocaluclaton.Operator
+
+/*
+	Adds players to the registry based on input given
+*/
+
+func AddPlayers(w http.ResponseWriter, r *http.Request) {
+	var p []elocaluclaton.Player
+
+	err := json.NewDecoder(r.Body).Decode(&p)
+	if err != nil {
+		return
+	}
+	println("Players retrived ", len(p))
+	errors := ""
+	hasErrors := false
+	for _, x := range p {
+		err := operator.CreateNewPlayer(x.Tag, x.Elo)
+		if err != nil {
+			errors += err.Error() + "\n"
+			hasErrors = true
+		}
+
+	}
+	if hasErrors {
+		http.Error(w, errors, http.StatusBadRequest)
+	}
+}
 
 func main() {
 	operator = elocaluclaton.CreateInMemoryOperator()
@@ -12,8 +42,12 @@ func main() {
 	operator.CreateNewPlayer("paul", 1000)
 	err := operator.RunMatch("paul", "chris", elocaluclaton.WIN)
 	if err != nil {
-		panic(err.Error())
+		log.Println(err.Error())
 	}
 	operator.RetrievePlayerInformation("paul")
 	operator.RetrievePlayerInformation("chris")
+
+	http.HandleFunc("/add", AddPlayers)
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
