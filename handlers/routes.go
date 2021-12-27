60800330d5e8db04842afc4f5e28fb632610d9be
Chris Smith <chris.smith@ahead.com>
2021-12-27 14:01:17 -0500

    Add serving a basic index page

--- handlers/routes.go
+++ handlers/routes.go
@@ -9,7 +9,7 @@ func notFound() http.HandlerFunc {
 }
 
 func (s *defaultServer) routes() {
-	//s.router.PathPrefix("/").HandlerFunc(s.serveIndexPage()).Methods.(http.MethodGet)
+	s.router.PathPrefix("/").HandlerFunc(s.serveIndexPage()).Methods(http.MethodGet)
 	s.router.PathPrefix("/add").HandlerFunc(s.addPlayers()).Methods(http.MethodPost)
 	s.router.PathPrefix("/decide").HandlerFunc(s.decide()).Methods(http.MethodPost)
 	s.router.PathPrefix("/updateElo").HandlerFunc(s.updatePlayerElo()).Methods(http.MethodPost)
