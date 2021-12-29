12a9d965390e3119dd69b7099369e56fd869ab33
Chris Smith <chris.smith@ahead.com>
2021-12-29 10:12:18 -0500

    Add client side validation for fields

--- static/js/app.js
+++ static/js/app.js
@@ -1,7 +1,18 @@
 document.getElementById("add-player-btn").addEventListener("click",(evt)=>{
     const playerName = document.getElementById("add-player-name").value;
     const playerElo = document.getElementById("add-player-elo").value;
-
+    if(playerName === ""){
+        updateNotification("Player Tag cannot be empty")
+        return
+    }
+    if(playerElo === ""){
+        updateNotification("Player Elo cannot be empty")
+        return
+    }
+    if(!parseInt(playerElo)){
+        updateNotification("Elo must be a number")
+        return
+    }
     console.log("Add Player Button Clicked!!!" +"Name: " +playerName +" elo: " + playerElo);
 
     addPlayer(playerName, playerElo)
@@ -9,7 +20,18 @@ document.getElementById("add-player-btn").addEventListener("click",(evt)=>{
 document.getElementById("update-player-btn").addEventListener("click",(evt)=>{
     const playerName = document.getElementById("add-player-name").value;
     const playerElo = document.getElementById("add-player-elo").value;
-
+    if(playerName === ""){
+        updateNotification("Player Tag cannot be empty")
+        return
+    }
+    if(playerElo === ""){
+        updateNotification("Player Elo cannot be empty")
+        return
+    }
+    if(!parseInt(playerElo)){
+        updateNotification("Elo must be a number")
+        return
+    }
     console.log("Update Player Button Clicked!!!" +"Name: " +playerName +" elo: " + playerElo);
 
     updatePlayer(playerName, playerElo)
@@ -17,7 +39,10 @@ document.getElementById("update-player-btn").addEventListener("click",(evt)=>{
 })
 document.getElementById("get-player-btn").addEventListener("click",(evt)=>{
     const playerName = document.getElementById("get-player-name").value;
-
+    if(playerName ===""){
+        updateNotification("Player Tag cannot be empty")
+        return 
+    }
     console.log("Get Player Button Clicked!!!" +"Name: " + playerName);
     getPlayer(playerName)
     .then((player) =>{ updateNotification(JSON.stringify(player))})
@@ -26,6 +51,15 @@ document.getElementById("decide-btn").addEventListener("click",(evt)=>{
     const playerNameA = document.getElementById("decide-player-a").value;
     const playerNameB = document.getElementById("decide-player-b").value;
     const result = document.getElementById("decide-result").value;
+
+    if(playerNameA === ""){
+        updateNotification("Player A Tag cannot be empty!")
+        return
+    }
+    if(playerNameB === ""){
+        updateNotification("Player B Tag cannot be empty!")
+    }
+    
     console.log("Decide Match Button Clicked!!!" +"NameA: " + playerNameA + " NameB" + playerNameB + " Result:" + result);
     decideMatch(playerNameA, playerNameB, result)
 })
